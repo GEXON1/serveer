0b96127e842424a10e926bf13c7efcba4238b626
Erik Shikhverdiev <erikshikhverdiev@Eriks-MacBook-Pro.local>
2023-09-17 19:25:31 -0400

    remove log

--- index.js
+++ index.js
@@ -15,9 +15,6 @@ app.post('/user', jsonParser, (req, res) => {
     console.log(req.body)
     res.status(200).json({user: {name: req.body.name}})
 })
-
-console.log(1)
-
   
 app.listen(3000, () => {
     console.log("server is initializaed")
