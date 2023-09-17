cd36705ea00155114a0b00d6e9a0c04d68c73193
Erik Shikhverdiev <erikshikhverdiev@Eriks-MacBook-Pro.local>
2023-09-17 19:20:36 -0400

    dfdsfds

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const bodyParser = require('body-parser')
+
+const app = express()
+
+const jsonParser = bodyParser.json()
+
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+app.get('/user', (req, res) => {
+    res.status(200).json({user: {name: "semyon"}})
+})
+
+app.post('/user', jsonParser, (req, res) => {
+    console.log(req.body)
+    res.status(200).json({user: {name: req.body.name}})
+})
+
+
+  
+app.listen(3000, () => {
+    console.log("server is initializaed")
+})
